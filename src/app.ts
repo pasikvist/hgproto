74dff7bbba797b8d4afa73955263f34f7bd9de0c
pasikvist <pasi.kvist@iki.fi>
2023-03-18 10:47:09 +0200

    First version of 'Hello Typescript'

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+let message: string = 'Hello, Typescript!';
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
